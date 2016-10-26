c786ff56f84f70fda376f8088e0c3f4ba9e183e9
Daniel Becker <dbecker4130@gmail.com>
2016-10-26 11:57:55 -0700

    working on appending projectInfo to html

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,3 +8,32 @@ function Project (options) {
   this.projectLink = options.projectLink;
   this.body = options.body;
 };
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  // clones new article template section
+
+// 1. title
+// 2. category
+// 3. date
+// 4. project link
+// 5. body
+
+  $newProject.find('h1').text(this.title);
+  $newProject.attr('data-category', this.category);
+
+  $newProject.find('section').html(this.body);
+  $newProject.find('a:contains("Project")').attr('href', this.projectLink);
+
+  $newProject.removeClass('template');
+
+  return $newProject;
+};
+
+projectInfo.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+
+projects.forEach(function(article) {
+  $('.projects').append(project.toHtml());
+});
