0ba015ca8d1dd6e846979c0af466cf721fe89293
Daniel Becker <dbecker4130@gmail.com>
2016-10-26 10:03:23 -0700

    project constructor function created

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,18 +1,10 @@
 'use strict';
-var articles = [];
+var projects = [];
 
-function Article (options) {
-/* TODO: This is our Model constructor. It will take in
-   our source data from blogArticles and instantiate a
-   new Object according to this new definition. options is
-   a placeholder for the object that will ultimately be
-   passed in. Use all of the properties in blogArticles
-   to populate the new Article data that we'll use.  */
-  //  DONE
+function Project (options) {
   this.title = options.title;
   this.category = options.category;
-  this.author = options.author;
-  this.authorUrl = options.authorUrl;
-  this.publishedOn = options.publishedOn;
+  this.date = options.date;
+  this.projectLink = options.projectLink;
   this.body = options.body;
 };
--- Scripts/projectInfo.js
+++ Scripts/projectInfo.js
@@ -1,7 +1,7 @@
 'use strict';
 var projectInfo = [
   {
-    title:       '',
+    title:       'Project 1',
     category:    '',
     date:      '',
     projectLink:   '',
@@ -9,7 +9,7 @@ var projectInfo = [
   },
 
   {
-    title:       '',
+    title:       'Project 2',
     category:    '',
     date:      '',
     projectLink:   '',
@@ -17,7 +17,7 @@ var projectInfo = [
   },
 
   {
-    title:       '',
+    title:       'Project 3',
     category:    '',
     date:      '',
     projectLink:   '',
@@ -25,15 +25,15 @@ var projectInfo = [
   },
 
   {
-    title:       '',
+    title:       'Project 4',
     category:    '',
     date:      '',
     projectLink:   '',
     body:     '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
   },
-  
+
   {
-    title:       '',
+    title:       'Project 5',
     category:    '',
     date:      '',
     projectLink:   '',
