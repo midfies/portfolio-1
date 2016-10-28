df0412d693a0efaeb6d457e4e0791184852d53e9
Daniel Becker <dbecker4130@gmail.com>
2016-10-28 10:35:50 -0700

    app.js displays projects

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,20 +2,10 @@ var projects = [];
 
 function Project (options) {
   this.title = options.title;
-  this.date = options.date;
-  this.projectLink = options.projectLink;
   this.body = options.body;
 };
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
-  // clones new article template section
-
-// 1. title
-// 2. category
-// 3. date
-// 4. project link
-// 5. body
 
   var templateScript = $('#article-template').html();
   console.log(templateScript);
@@ -24,15 +14,6 @@ Project.prototype.toHtml = function() {
   console.log(this);
   return theTemplate(this);
 
-  $newProject.find('h1').text(this.title);
-  $newProject.attr('data-category', this.category);
-
-  $newProject.find('section').html(this.body);
-  $newProject.find('a:contains("Project")').attr('href', this.projectLink);
-
-  $newProject.removeClass('template');
-
-  return $newProject;
 };
 
 projectInfo.forEach(function(ele) {
@@ -40,5 +21,5 @@ projectInfo.forEach(function(ele) {
 });
 
 projects.forEach(function(article) {
-  $('.projects').append(project.toHtml());
+  $('#projects').append(article.toHtml());
 });
